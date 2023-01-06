c5be04b9503ac916383c8362b824efb8a52828c2
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-01-05 18:24:54 -0700

    Added config struct in state.rs file.

--- contracts/messaging/src/state.rs
+++ contracts/messaging/src/state.rs
@@ -5,9 +5,13 @@ use cosmwasm_std::Addr;
 use cw_storage_plus::Item;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
+// pub struct State {
+//     pub count: i32,
+//     pub owner: Addr,
+// }
+pub struct Config {
     pub owner: Addr,
+    pub greeting: String
 }
 
 pub const STATE: Item<State> = Item::new("state");
