8eb50867bc7780d86a36a3dec2528476fc97aa10
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-01-14 19:35:44 -0700

    Worked on index file.

--- cudos-messaging-dapp/config/defaults.ts
+++ cudos-messaging-dapp/config/defaults.ts
@@ -1,12 +1,13 @@
 import { assets } from 'chain-registry';
 import { AssetList, Asset } from '@chain-registry/types';
 
-export const chainName = 'cosmwasmtestnet';
-export const stakingDenom = 'umlg';
-export const feeDenom = 'uand';
+export const chainName = 'cudostestnet';
+export const stakingDenom = 'cudos';
+export const feeDenom = 'acudos';
 
-// export const cw20ContractAddress = 'wasm1p7vmrhl3s0fyl0m9hk2hlm7uuxq84hztur63n8ryh85chh30vt6q89shcv'
+export const cw20ContractAddress = 'wasm1p7vmrhl3s0fyl0m9hk2hlm7uuxq84hztur63n8ryh85chh30vt6q89shcv'
 export const messagingContractAddress = 'cudos14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9strccpl'
+export const rpcEndpoint = 'https://sentry1.gcp-uscentral1.cudos.org:36657/';
 
 export const chainassets: AssetList = assets.find(
     (chain) => chain.chain_name === chainName
@@ -15,3 +16,7 @@ export const chainassets: AssetList = assets.find(
 export const coin: Asset = chainassets.assets.find(
     (asset) => asset.base === stakingDenom
 ) as Asset;
+
+export const chainEndpoint = chains.find(
+    (chain) => chain.chain_name = chainName
+)?.apis?.rpc?.at(1)?.address ?? rpcEndpoint;
